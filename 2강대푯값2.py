a4edc4d2cb9370552b9e3f05c04cd0d958e738b2
eunewoo <ewchoi98@gmail.com>
2022-04-26 20:06:45 +0900

    Create 대푯값 and 뒤집은소수

---
+++ 2강대푯값2.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin=open("input.txt", "r")
+
+n = int(input())
+a = list(map(int, input().split()))
+
+sum = 0
+for i in a:
+    sum += i
+
+mean = round(sum / n) #mean = ЦђБе
+
+
+min = 2147000
+
+for i in range(n):
+    diff = abs(a[i] - mean)
+    
+    if diff < min:
+        min = diff
+        ans = i
+
+    if diff == min:
+        if a[i] > a[ans]:
+            ans = i
+
+print("%d %d" %(mean, ans + 1))
+
+
+
+
---
+++ 2강뒤집은소수.py
@@ -0,0 +1,42 @@
+import sys
+sys.stdin=open("input.txt", "r")
+
+n = int(input())
+a = list(map(int, input().split()))
+
+def reverse(x):
+    while(x % 10 == 0):
+        x = x // 10
+
+    x = str(x)
+        
+    oppo_num = ''
+    for j in x:
+        oppo_num = j + oppo_num
+
+    oppo_num = int(oppo_num)
+    return oppo_num
+
+def isPrime(x):
+    if x == 1:
+        return False
+
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+   
+    return True
+
+b = []
+for i in range(len(a)):
+    new_a = reverse(a[i])
+    a[i] = new_a
+    if isPrime(a[i]):
+        b.append(a[i])
+
+print(*b)
+
+
+    
+
+
